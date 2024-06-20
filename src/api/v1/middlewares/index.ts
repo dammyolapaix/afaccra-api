2ae7ad097e1e3957f50e9b20a06f095a17df1a0c
Dammy Ola <dammyolapaix@gmail.com>
2024-06-20 18:47:45 +0000

    Added localization en and fr

--- src/api/v1/middlewares/index.ts
+++ src/api/v1/middlewares/index.ts
@@ -1,5 +1,12 @@
 import asyncHandler from './async.middleware'
 import { errorHandler, notFound } from './error.middlewares'
 import validationMiddleware from './validation.middleware'
+import localizationMiddleware from './localization.middleware'
 
-export { asyncHandler, errorHandler, notFound, validationMiddleware }
+export {
+  asyncHandler,
+  errorHandler,
+  notFound,
+  validationMiddleware,
+  localizationMiddleware,
+}
--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,19 @@ import dotenv from 'dotenv'
 dotenv.config()
 import routes from './api/v1/routes'
 import { errorHandler, notFound } from './api/v1/middlewares'
-
+import i18next from 'i18next'
+import i18nextMiddleware from 'i18next-http-middleware'
 const app: Application = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+// Middleware initialization to handle language detection
+app.use(
+  i18nextMiddleware.handle(i18next, {
+    removeLngFromUrl: false, // Optional: Remove language code from URL
+  })
+)
 
 app.use(routes)
 app.use(notFound)
