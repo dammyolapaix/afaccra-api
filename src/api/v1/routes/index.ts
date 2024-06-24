fa5af84a982dd0e8991b665750b0f056507e5a95
Dammy Ola <dammyolapaix@gmail.com>
2024-06-24 23:24:56 +0000

    Add topic route

--- src/api/v1/routes/index.ts
+++ src/api/v1/routes/index.ts
@@ -2,11 +2,13 @@ import express from 'express'
 import { courseRoutes } from '../features/courses'
 import { authRoutes, authenticatedMiddleware } from '../features/users/auth'
 import { classRoutes } from '../features/classes'
+import { topicRoutes } from '../features/classes/topics'
 
 const router = express.Router()
 
 router.use('/api/v1/auth', authRoutes)
 router.use('/api/v1/courses', courseRoutes)
 router.use('/api/v1/classes', authenticatedMiddleware, classRoutes)
+router.use('/api/v1/topics', authenticatedMiddleware, topicRoutes)
 
 export default router
